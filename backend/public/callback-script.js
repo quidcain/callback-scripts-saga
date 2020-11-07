e16c84521bdd21c90b6f0504155194b7d61fa845
Daniel Radezky <danielradezky@gmail.com>
2020-11-07 18:03:16 +0300

    Set up express that serves a callback-script as a static file

---
+++ backend/public/callback-script.js
@@ -0,0 +1,21 @@
+var recsData = [
+  {
+    name: 'homeSplashBanner',
+    position: 'position1',
+    recommendation: ['21336','43213']
+  },{
+    name: 'homeSplashBanner',
+    position: 'position1',
+    recommendation: ['43213','31412']
+  },{
+    name: 'homeSplashBanner',
+    position: 'position2',
+    recommendation: ['341234','132421']
+  },{
+    name: 'cartBanner',
+    position: 'position1',
+    recommendation: ['341234','43213']
+  }
+];
+
+window.PeeriusCallbacks.smartRecs(recsData);
