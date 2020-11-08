d4edcff2bc211ee95e8ec29a9662371ff78797ab
Daniel Radezky <danielradezky@gmail.com>
2020-11-08 10:57:19 +0300

    Bundle js with webpack

---
+++ frontend/webpack.config.js
@@ -0,0 +1,22 @@
+const webpack = require('webpack');
+const path = require('path');
+
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'assets/dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      }
+    ]
+  }
+};
